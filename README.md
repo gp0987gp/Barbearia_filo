d8d1938a95e4dc24b45d50f585861c931f1c0d57
gp0987gp <gp0987gp@gmail.com>
2024-01-30 08:07:16 -0300

    att

---
+++ README.md
@@ -0,0 +1,40 @@
+## Como utilizar este repositório
+
+1. Clone este repositório para o seu ambiente de desenvolvimento local utilizando o comando:
+```
+ git clone https://github.com/gp0987gp/barbearia_filo.git
+```
+2. Acesse o diretório do projeto:
+```
+cd barbearia_filo
+```
+3. Inicializando o Repositorio
+```
+ composer update
+
+```
+4. Copiando o env. do exemplo
+```
+copy .env.example .env
+```
+
+5. Acessar o Visual Studio Code
+```
+code .
+```
+
+## Insomnia
+[![Run in Insomnia}](https://insomnia.rest/images/run.svg)](https://insomnia.rest/run/?label=barbearialb&uri=https%3A%2F%2Fgithub.com%2FClaudiodoSenai%2Fbarbearialb%2Fblob%2Fmain%2FInsomniaDefinitivo%2520.json)
+
+## Licença
+
+Este projeto está licenciado sob a [MIT License](LICENSE), o que significa que você pode utilizá-lo livremente para fins educacionais, comerciais ou pessoais.
+
+## Contato
+Em caso de dúvidas, sugestões ou problemas, sinta-se à vontade para entrar em contato através do email: claudiorobertojunior5@gmail.com
+
+**Aviso**: Certifique-se de substituir "gp0987gp" no link de clonagem e "gp0987gp@gmail.com" na seção de contato pelos valores correspondentes do seu próprio usuário do GitHub e endereço de e-mail.
+
+Este é apenas um modelo básico de README.md, você pode adaptar e melhorar conforme suas necessidades específicas de projeto. Certifique-se de incluir informações importantes sobre como configurar, executar e testar os exercícios, bem como qualquer outra documentação relevante para tornar o repositório mais amigável e útil para outros desenvolvedores."# Barbearia_filo" 
+"# Barbearia_filo" 
+"# Barbearia_filo" 
---
+++ app/Http/Requests/AgendaUpdateFormRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AgendaUpdateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'max:120|min:5 ',
+            'email' => 'max:120|email:rfc,|unique:clientes,email,'. $this->id,
+            'cpf' => '|max:11|min:11|unique:clientes,cpf,'. $this->id,
+            'senha' => ''
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'error' => $validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.max' => 'O campo nome deve conter no máximo 120 caracteres',
+            'nome.min' => 'O campo nome deve conter no minimo 5 caracteres',
+
+            'email.max' => 'O campo e-mail deve conter no máximo 120 caracteres',
+            'email.email' => 'Formato de email invalido',
+            'email.unique' => 'E-mail já cadastrado',
+
+            'cpf.max' => 'CPF deve conter no máximo 11 caracteres',
+            'cpf.min' => 'CPF deve conter no mínimo 11 caracteres',
+            'cpf.unique' => 'CPF Já cadastrado no sistema',
+
+            'senha.required' => 'Senha obrigatoria'
+        ];
+    }
+}
+
